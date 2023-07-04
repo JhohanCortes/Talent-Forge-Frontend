7a57a68c4ca4ae8f991121e29e326994f84e15fe
ERehbani <elianleesin@gmail.com>
2023-07-04 18:49:28 -0300

    Formulario, falta país

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import Landing from './Components/LandingPage/Landing';
+import "./App.css";
+import Form from "./Components/Form/Form";
+import Landing from "./Components/LandingPage/Landing";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Landing/>
+        {/* <Landing /> */}
+        <Form />
       </header>
     </div>
   );
