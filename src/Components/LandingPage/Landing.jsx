432c37da314d249aa779ddb90d4f3886c7d34818
unknown <sebadevalbornoz@gmail.com>
2023-07-04 17:00:19 -0300

    Landing 80%

--- src/Components/LandingPage/Landing.jsx
+++ src/Components/LandingPage/Landing.jsx
@@ -1,40 +1,76 @@
-import './style.css'
+import React, {useState} from 'react';
 
-const Landing = ( ) => {
+import './style.css'
+import logo1 from './imgs/logo1.jpg';
+import imgpres from './imgs/_34feeb5f-5418-4f15-ae5b-a0e6f9cd7285-removebg-preview.png'
+import empresas from './imgs/empresas.jpg'
+
+const Landing = () => {
+    const [activeButton,setActiveButton] = useState('idiomas');
+
+    const renderContent = () => {
+        if (activeButton === 'idiomas') {
+            return <div> ACA VAN LOS IDIOMAS</div>
+        } else {
+            return <div> ACA VAN LOS LENGUAJES DE PROGRAMACION</div>
+        }
+    }
+
+    const handleButtonClick = (button) => {
+        setActiveButton(button);
+    }
+
     return (
         <div className='landing'> 
             <div className="header">
-                <h2>Talent Forgers</h2>
+                <img src={logo1} alt="" />
                 <div className="buttons">
-                    <button>Registrarse</button>
-                    <button>Entrar</button>
+                    <button id='register'>Registrarse</button>
+                    <button id='login'>Entrar</button>
                 </div>
             </div>
             
             <div className="presentation">
                 <div className="presentationContainer">
-                    <h3>Habla un idioma en tan solo 10 minutos al dia`</h3>
+                    <h3>Habla un idioma en tan solo 10 minutos al dia!</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta esse amet modi. Dignissimos, beatae autem quisquam nobis esse vel atque maxime sapiente sequi eum earum veniam architecto iure quaerat repellat!</p>
+                    <button id='aprende-Gratis'>Aprende Gratis</button>
                 </div>
-                <h2>Aca v ala imagen</h2>
-                <img src="" alt="" />
+                <img src={imgpres} alt="" />
             </div>
 
             <div className="whiteBack">
                 <div className="selector">
-                    <button>Idiomas</button>
-                    <button>Programacion</button>
-                    {/* ACA VA EL COMPONENTE DE RENDERIZADO DE CURSOS */}
+                    <h2>Quiero Aprender</h2>
+                    <div className="botones">
+                        <button id='idiomas' 
+                            className={activeButton === 'idiomas' ? 'active' : ''}
+                            onClick={() => handleButtonClick('idiomas')}
+                            >Idiomas</button>
+
+                        <button id='programacion'
+                            className={activeButton === 'programacion' ? 'active' : ''}
+                            onClick={() => handleButtonClick('programacion')} 
+                        >Programacion</button>
+                    </div>
+                    
+                    {renderContent() /*ESTO DEBERIA RENDERIZAR CIERTO CONTENIDO */}
+
                 </div>
                 <div className="mentores">
                     <h2>Mentores</h2>
                     {/* COMPONENTE DE RENDERIZADO DE PROFESORES */}
                 </div>
                 
-                <div className="paraEmpresas">
-                    <h2>Aca va una imagen</h2>
-                    <h3>Buscas soluciones de idiomas para tu empresa?</h3>
-                    <h3 id='textoVioleta'>Averigua como podemos ayudarte</h3>
+                <div className="empresas">
+                    <h2>Talent Forge para empresas</h2>
+                    <div className="container">
+                        <img src={empresas} alt="" />
+                        <div className="infoContainer">
+                            <h3>Buscas soluciones de idiomas para tu empresa?</h3>
+                            <h3 id='textoVioleta'>Averigua como podemos ayudarte</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
             
@@ -42,19 +78,24 @@ const Landing = ( ) => {
                 <div className="contact-home">
                     <h3>Contact</h3>
                     <h3>Home</h3>
+                    <h3>Ubication</h3>
                 </div>
                 <div className="logo-redes">
-                    <h2>Aca va el logo</h2>
-                    <div className="redes">
-                        <h3><a href="">Instagram</a></h3>
-                        <h3><a href="">Facebook</a></h3>
-                        <h3><a href="">YouTube</a></h3>
-                        <h3><a href="">Linkedin</a></h3>
-                        <h3><a href="">Whatsapp</a></h3>
+                    <img src="src/Components/LandingPage/imgs/opcion4.jpg" alt="" srcset="" />
+                    <div className="logo-redes">
+                        <img src={logo1} alt="" />
+                        <div className="redes">
+                            <h3><a href="#">Instagram</a></h3>
+                            <h3><a href="#">Facebook</a></h3>
+                            <h3><a href="#">YouTube</a></h3>
+                            <h3><a href="#">Linkedin</a></h3>
+                            <h3><a href="#">Whatsapp</a></h3>
+                        </div>
                     </div>
                 </div>
                 <div className="about">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum eligendi illo, ducimus accusantium quidem minima, et molestiae sint odit eveniet eos. Autem impedit beatae veniam necessitatibus, corrupti explicabo fugit quod.</p>
+                    <h3>About Us</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum eligendi illo, ducimus accusantium quidem </p>
                 </div>
             </div>
         </div>
